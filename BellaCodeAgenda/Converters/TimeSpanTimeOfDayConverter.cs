d498fdcd557e4fd8e0ce2d82668f0109da888a55
BellaCode <info@bellacode.com>
2014-10-16 00:51:37 -0700

    Determine how to set am pm ambiguous hours

--- BellaCodeAgenda/Converters/TimeSpanTimeOfDayConverter.cs
+++ BellaCodeAgenda/Converters/TimeSpanTimeOfDayConverter.cs
@@ -91,10 +91,80 @@ namespace BellaCodeAgenda.Converters
 
             int hours = int.Parse(hoursText);
 
-            // If there is no hint, then I use current AM/PM
-            if (isPmHint == null)
+            int minutes = 0;
+            if (!string.IsNullOrEmpty(minutesText))
             {
-                isPmHint = DateTime.Now.Hour >= 12;
+                minutes = int.Parse(minutesText);
+            }
+
+            if (hours > 24)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+            if (minutes > 60)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+
+            // If there is no hint and the user entered a number that isn't clearly AM/PM on a 24-hour clock
+            if (isPmHint == null && hours > 0 && hours <= 12)
+            {
+                var amHour = hours%12;
+                var pmHour = hours%12 + 12;
+
+                var nowHour = DateTime.Now.Hour;
+                var isAm = nowHour < 12;
+
+                if (isAm && nowHour > amHour)
+                {
+                    isPmHint = true;
+                }
+                else if (!isAm && nowHour <= pmHour)
+                {
+                    isPmHint = true;
+                }
+                else if (hours == 12)
+                {
+                    isPmHint = false;
+                }
+
+
+#if false // brute force conditional logic algorithm
+                var nowHour = DateTime.Now.Hour;
+
+                // If it is 12 AM, then I never adjust the time to PM.
+                if (nowHour != 0)
+                {
+                    // If it is AM
+                    if (nowHour < 12)
+                    {
+                        // I consider "12" and any hours that are before the current hour to mean PM                                                
+                        if (hours == 12 || nowHour - hours > 0)
+                        {
+                            isPmHint = true;
+                        }
+                    }
+
+                    // If it is PM
+                    if (nowHour >= 12)
+                    {
+                        var adjustedNowHour = nowHour - 12;
+
+                        // I treat "12" as a special case because it is larger than other 12-hour clock PM numbers
+                        if (hours == 12)
+                        {
+                            isPmHint = (adjustedNowHour == 0);
+                        }
+                        // I consider any hours that are on or after the current hour to mean PM
+                        else if (hours - adjustedNowHour >= 0)
+                        {
+                            isPmHint = true;
+                        }
+                    }
+                }
+#endif
             }
 
             // If there is a hint that this is a PM time, I add 12 hours.
@@ -102,18 +172,16 @@ namespace BellaCodeAgenda.Converters
             {
                 hours += 12;
             }
-            // If there is a hint that this is AM, I conver 12 to 0.
+            // If there is a hint that this is AM, I convert 12 to 0.
             else if (isPmHint == false && hours == 12)
             {
                 hours = 0;
             }
-                      
 
-            int minutes = 0;
-            if (!string.IsNullOrEmpty(minutesText))
-            {
-                minutes = int.Parse(minutesText);
-            }
+            // Now Hour: 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
+            // PM:       
+
+            
 
             var timeSpan = TimeSpan.FromHours(hours) + TimeSpan.FromMinutes(minutes);
 
