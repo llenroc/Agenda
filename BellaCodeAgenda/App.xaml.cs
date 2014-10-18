64775ca4c7116be9c3f16cf23596fd040ecb68e0
BellaCode <info@bellacode.com>
2014-10-18 15:37:07 -0700

    Embedded dependent DLLs
    Makes this a single EXE.

--- BellaCodeAgenda/App.xaml.cs
+++ BellaCodeAgenda/App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +15,32 @@ namespace BellaCodeAgenda
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+            {
+                try
+                {
+                    if (args.Name.IndexOf("resources", StringComparison.OrdinalIgnoreCase) != -1)
+                    {
+                        return null;
+                    }
+
+                    String resourceName = "BellaCodeAgenda.Embedded." + new AssemblyName(args.Name).Name + ".dll";                    
+
+                    using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+                    {
+                        Byte[] assemblyData = new Byte[stream.Length];
+                        stream.Read(assemblyData, 0, assemblyData.Length);
+                        return Assembly.Load(assemblyData);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.ToString());
+                    return null;
+                }
+            };
+        }        
     }
 }
